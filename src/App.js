ce90999e71d27fd116e72ffa990caca485062f37
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2023-04-07 14:06:59 +0300

    create contact component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, {lazy, Suspense} from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 const Footer = lazy(() => import('./components/footer/footer.component'));
+const Contact = lazy(() => import('./components/contact/contact.component'));
 
 function App() {
   return (
     <div>
-        <h1>Hello</h1>
-
+      <h1>Hello</h1>
+      
       <Suspense fallback={<div/>}>
+        <Contact />
         <Footer />
       </Suspense>
     </div>
---
+++ src/components/contact/contact.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './contact.styles.css';
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="section-title text-center">
+                            <span className="subtitle">Contact</span>
+                            <h2 className="title">Contact With Me</h2>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    );
+};
+export default Contact;
